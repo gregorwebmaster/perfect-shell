2f38e0530c6647b41d13ff02ace4b1c150f4fdad
Grzegorz Jaworski <jaworski@blaudirekt.de>
2025-02-17 19:32:41 +0100

    update nvim configuration

---
+++ backup/tmux/nvim-help.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Define color codes
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}### Neovim Help ###${NC}"
+echo
+
+echo -e "${YELLOW}#### Default Shortcuts ####${NC}"
+echo
+
+# Default Neovim Shortcuts
+echo -e "${GREEN}:w | Save the current file${NC}"
+echo -e "${GREEN}:q | Quit Neovim${NC}"
+echo -e "${GREEN}:wq | Save and quit${NC}"
+echo -e "${GREEN}:wqa | Save all and quit Neovim${NC}"
+echo -e "${GREEN}:e filename | Edit a specific file${NC}"
+echo -e "${GREEN}:split | Split the window horizontally${NC}"
+echo -e "${GREEN}:vsplit | Split the window vertically${NC}"
+echo -e "${GREEN}Ctrl; w; h/j/k/l | Navigate between split windows${NC}"
+echo -e "${GREEN}dd | Delete the current line${NC}"
+echo -e "${GREEN}yy | Yank (copy) the current line${NC}"
+echo -e "${GREEN}p | Paste after the cursor${NC}"
+echo -e "${GREEN}u | Undo the last action${NC}"
+echo -e "${GREEN}Ctrl; r | Redo the last undone action${NC}"
+echo
+
+echo -e "${YELLOW}#### Plugin Shortcuts ####${NC}"
+echo
+
+# Telescope Plugin Shortcuts
+echo -e "${CYAN}##### Telescope #####${NC}"
+echo -e "${GREEN}<Leader>; ff | Find files using Telescope${NC}"
+echo -e "${GREEN}<Leader>; fg | Live grep using Telescope${NC}"
+echo
+# You can add more Telescope shortcuts here if defined
+
+# Neo-tree Plugin Shortcuts
+echo -e "${CYAN}##### Neo-tree #####${NC}"
+echo -e "${GREEN}<Leader>; n | Reveal the filesystem in Neo-tree on the left${NC}"
+echo -e "${GREEN}<Leader>; g | Open Neo-tree in a floating window showing git status${NC}"
+echo -e "${GREEN} a | Create a new file${NC}"
+echo -e "${GREEN} c | Copy file/directory${NC}"
+echo -e "${GREEN} d | Delete file/directory${NC}"
+echo
+
+echo -e "${BLUE}### End of Help ###${NC}"
+
--- backup/tmux/tmux-help.sh
+++ backup/tmux/tmux-help.sh
@@ -15,6 +15,7 @@ echo
 # General Section
 echo -e "${GREEN}Prefix - Ctrl + Space | Sets Ctrl + Space as the tmux prefix key.${NC}"
 echo -e "${GREEN}Prefix, r | Reloads the tmux configuration file.${NC}"
+echo -e "${GREEN}Prefix, : | Enter into command line.${NC}"
 echo
 echo -e "${GREEN}Prefix, Ctrl + s | Saves the current sessions.${NC}"
 echo -e "${GREEN}Prefix, Ctrl + r | Restores previously saved sessions.${NC}"
@@ -26,8 +27,14 @@ echo
 
 # Windows Section
 echo -e "${GREEN}Prefix, c | Creates a new window.${NC}"
+echo -e "${GREEN}Prefix, , | Rename a window.${NC}"
+echo -e "${GREEN}Prefix,  | Creates a new window.${NC}"
+
+
 echo -e "${GREEN}Shift+Alt+H | Switches to the previous window.${NC}"
 echo -e "${GREEN}Shift+Alt+L | Switches to the next window.${NC}"
+echo
+echo -e "${GREEN}Prefix, f | find window by name.${NC}"
 echo
 
 echo -e "${YELLOW}#### Panes ####${NC}"
@@ -44,14 +51,20 @@ echo -e "${GREEN}Prefix, ! | Exits zoomed view to return to normal view.${NC}"
 echo -e "${GREEN}Prefix, x | Kills (closes) the current pane.${NC}"
 echo
 
-echo -e "${YELLOW}#### Console ####${NC}"
+echo -e "${YELLOW}#### Copy mode ####${NC}"
 echo
 
-# Console
+# Copy mode
 echo -e "${GREEN}Prefix, [ | Enter to copy mode.${NC}"
-echo -e "${GREEN}copy-mode v,  | Begin selection.${NC}"
-echo -e "${GREEN}copy-mode Ctrl+v,  | Rectangle selection.${NC}"
-echo -e "${GREEN}copy-mode y,  | Copy selection and exit copy mode.${NC}"
+echo
+echo -e "${GREEN}<in-copy-mode> / | Press / to start a forward search.${NC}"
+echo -e "${GREEN}<in-copy-mode> ? | Press ? to start a backward search.${NC}"
+echo -e "${GREEN}<in-copy-mode> n | Press n to go to the next search result.${NC}"
+echo -e "${GREEN}<in-copy-mode> Shift+n | Press Shift+n to go to the previous search result.${NC}"
+echo
+echo -e "${GREEN}<in-copy-mode>  v,  | Begin selection.${NC}"
+echo -e "${GREEN}<in-copy-mode>  Ctrl+v,  | Rectangle selection.${NC}"
+echo -e "${GREEN}<in-copy-mode>  y,  | Copy selection and exit copy mode.${NC}"
 echo
 
 echo -e "${YELLOW}#### Additional Commands ####${NC}"
